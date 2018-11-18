b8baeb697afeb9405da89d8783a3444bddb1d9d2
Kimserey <kimserey.lam@groomgy.com>
2018-11-18 22:43:06 +0000

    one ore controller

--- DummyAPI/Controllers/CompaniesController.cs
+++ DummyAPI/Controllers/CompaniesController.cs
@@ -5,17 +5,17 @@ using Microsoft.Extensions.Logging;
 namespace DummyAPI.Controllers
 {
     [ApiController]
-    [Route("api/persons")]
-    public class PersonsController : ControllerBase
+    [Route("api/companies")]
+    public class CompaniesController : ControllerBase
     {
         public ActionResult<Person[]> GetAll([FromServices] ILogger<PersonsController> logger)
         {
-            logger.LogInformation("Get all persons.");
+            logger.LogInformation("Get all companies.");
 
             return new[] {
-                new Person { Name = "Kim" },
-                new Person { Name = "Tom" },
-                new Person { Name = "Sam" },
+                new Person { Name = "Microsoft" },
+                new Person { Name = "Thomson Reuters" },
+                new Person { Name = "Bloomberg" },
             };
         }
     }
