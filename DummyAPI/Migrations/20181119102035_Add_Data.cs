ce56c6e835058c1a615ea07654fbbed8b9a3d89c
Kimserey <kimserey.lam@groomgy.com>
2018-11-19 10:28:01 +0000

    Added migration to add author and blog

---
+++ DummyAPI/Migrations/20181119102035_Add_Data.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DummyAPI.Migrations
+{
+    public partial class Add_Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Authors", "Name", new[] { "Kim", "Tom", "Sam" });
+            migrationBuilder.InsertData("Blogs", "Url", new[] { "https://kims.blog.com", "https://toms.blog.com", "https://sams.blog.com" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
