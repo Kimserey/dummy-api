1d11d84a6d218be97853b82deb31a1186c3a6dc1
Kimserey <kimserey.lam@groomgy.com>
2018-11-19 10:59:47 +0000

    Added posts migration

--- DummyAPI/Migrations/20181119102726_Add_Posts.cs
+++ DummyAPI/Migrations/20181119102726_Add_Posts.cs
@@ -6,7 +6,25 @@ namespace DummyAPI.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(@"
+                INSERT INTO Posts (AuthorId, BlogId, Title, Content)
+                VALUES (1, 1, 'Just Photoshop out the dog', 'There is too much white space I have printed it out, but the animated gif is not moving could you rotate the picture to show the other side of the room?'); 
+            ");
 
+            migrationBuilder.Sql(@"
+                INSERT INTO Posts (AuthorId, BlogId, Title, Content)
+                VALUES (1, 1, 'Just Photoshop out the dog', 'There is too much white space I have printed it out, but the animated gif is not moving could you rotate the picture to show the other side of the room?'); 
+            ");
+
+            migrationBuilder.Sql(@"
+                INSERT INTO Posts (AuthorId, BlogId, Title, Content)
+                VALUES (2, 2, 'Just Photoshop out the dog', 'There is too much white space I have printed it out, but the animated gif is not moving could you rotate the picture to show the other side of the room?'); 
+            ");
+
+            migrationBuilder.Sql(@"
+                INSERT INTO Posts (AuthorId, BlogId, Title, Content)
+                VALUES (3, 3, 'Just Photoshop out the dog', 'There is too much white space I have printed it out, but the animated gif is not moving could you rotate the picture to show the other side of the room?'); 
+            ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
